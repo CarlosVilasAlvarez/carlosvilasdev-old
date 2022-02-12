a491896779c8ee3dce903a2939fd932275b2a836
CarlosVilasAlvarez <carlosvilasalvarez@gmail.com>
2022-02-12 01:10:36 +0100

    [blog-posts-cards] render cards in the blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,36 @@
-function BlogHome(): JSX.Element {
+import { GetStaticProps } from 'next';
+import BlogPostCard from '../../components/shared/BlogPostCard/BlogPostCard';
+import { getAllPosts, Post } from '../../lib/blogApi';
+
+type BlogHomeProps = {
+    posts: Post[];
+};
+
+function BlogHome({ posts }: BlogHomeProps): JSX.Element {
     return (
         <div>
             <h1>Blog Home</h1>
+            {posts.map((post) => {
+                return (
+                    <BlogPostCard
+                        slug={post.slug}
+                        title={post.metadata.title}
+                        description={post.metadata.description}
+                        published_date={post.metadata.published_date}
+                    />
+                );
+            })}
         </div>
     );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+    const posts = await getAllPosts();
+    return {
+        props: {
+            posts,
+        },
+    };
+};
 
 export default BlogHome;
