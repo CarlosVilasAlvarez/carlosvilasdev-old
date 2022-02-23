e3aa3192512931655b3b0d1a638014972ab0f68f
CarlosVilasAlvarez <carlosvilasalvarez@gmail.com>
2022-02-23 22:16:40 +0100

    display posts in grid

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import BlogPostCard from '../../components/shared/BlogPostCard/BlogPostCard';
 import { getAllPosts, Post } from '../../lib/blogApi';
+import styles from './index.module.scss';
 
 type BlogHomeProps = {
     posts: Post[];
@@ -8,22 +9,26 @@ type BlogHomeProps = {
 
 function BlogHome({ posts }: BlogHomeProps): JSX.Element {
     return (
-        <div>
-            <h1>Blog Home</h1>
-            {posts.map((post) => {
-                return (
-                    <BlogPostCard
-                        key={post.slug}
-                        slug={post.slug}
-                        title={post.metadata.title}
-                        description={post.metadata.description}
-                        card_image={post.metadata.card_image}
-                        published_date={post.metadata.published_date}
-                        author={post.metadata.author}
-                    />
-                );
-            })}
-        </div>
+        <main className={styles.blog_page}>
+            <div className={styles.filters_section}></div>
+            <div className={styles.grid_wrapper}>
+                <div className={styles.posts_grid}>
+                    {posts.map((post) => {
+                        return (
+                            <BlogPostCard
+                                key={post.slug}
+                                slug={post.slug}
+                                title={post.metadata.title}
+                                description={post.metadata.description}
+                                card_image={post.metadata.card_image}
+                                published_date={post.metadata.published_date}
+                                author={post.metadata.author}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </main>
     );
 }
 
