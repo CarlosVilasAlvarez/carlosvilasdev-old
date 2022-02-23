3dd7d46644c30b259b91f09e1ce87debf456a04d
CarlosVilasAlvarez <carlosvilasalvarez@gmail.com>
2022-02-23 01:42:20 +0100

    add new properties to post metadata

--- src/components/shared/BlogPostCard/BlogPostCard.tsx
+++ src/components/shared/BlogPostCard/BlogPostCard.tsx
@@ -1,5 +1,6 @@
 import styles from './blogpostcard.module.scss';
 import Link from 'next/link';
+import Image from 'next/image';
 
 type BlogPostCardProps = {
     slug: string;
@@ -7,6 +8,7 @@ type BlogPostCardProps = {
     description: string;
     published_date: string;
     card_image: string;
+    author: string;
 };
 
 function BlogPostCard({
@@ -15,19 +17,26 @@ function BlogPostCard({
     description,
     published_date,
     card_image,
+    author,
 }: BlogPostCardProps): JSX.Element {
     return (
         <Link href={`/blog/posts/${slug}`}>
             <a className={styles.post_card__link_wrapper}>
                 <div className={styles.post_card}>
                     <div className={styles.post_image_wrapper}>
-                        <img src={card_image} alt={`Image of the post ${title}`} />
+                        <Image
+                            src={card_image}
+                            alt={`Image of the post ${title}`}
+                            layout="fill"
+                            objectFit="cover"
+                        />
                     </div>
                     <div className={styles.post_card_body}>
                         <h2>{title}</h2>
                         <p className={styles.post_description}>{description}</p>
                     </div>
                     <div className={styles.post_footer}>
+                        <p>{author}</p>
                         <p>{published_date}</p>
                     </div>
                 </div>
