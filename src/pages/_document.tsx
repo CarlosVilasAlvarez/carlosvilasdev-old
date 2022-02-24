3b8f309fbf2246621e1db212313e8812a9ddf25f
CarlosVilasAlvarez <carlosvilasalvarez@gmail.com>
2022-02-24 01:24:50 +0100

    fix _document head warnings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,6 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <title>Carlos Vilas</title>
-                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
                         rel="stylesheet"
