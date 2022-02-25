191a840c7d3b6fd9aeb8095d42cb6b7b8e1cd939
CarlosVilasAlvarez <carlosvilasalvarez@gmail.com>
2022-02-25 02:39:10 +0100

    add basic and ugly code to test the search works properly

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -3,7 +3,9 @@ import BlogPostCard from '../../components/shared/BlogPostCard/BlogPostCard';
 import { getAllPosts, Post } from '../../lib/blogApi';
 import styles from './index.module.scss';
 import Select from '../../components/shared/Select/Select';
+import FullTextSearch from '../../components/shared/FullTextSearch/FullTextSearch';
 import { useState } from 'react';
+import MiniSearch from 'minisearch';
 
 type BlogHomeProps = {
     posts: Post[];
@@ -12,8 +14,10 @@ type BlogHomeProps = {
 function BlogHome({ posts }: BlogHomeProps): JSX.Element {
     const [displayed_posts, setDisplayedPosts] = useState(posts);
     const categories = Array.from(new Set(posts.map((post) => post.metadata.tags).flat()));
-    categories.unshift('all'); // Add 'all' category to the beginning of the array so it's displayed in the select by default
+    // Add 'all' category to the beginning of the array so it's displayed in the select by default
+    categories.unshift('all');
 
+    // Handle the change of categories in select element
     const onCategorySelect = (event: React.ChangeEvent<HTMLSelectElement>): void => {
         const selected_category = event.target.value;
         if (selected_category === 'all') {
@@ -25,10 +29,46 @@ function BlogHome({ posts }: BlogHomeProps): JSX.Element {
         }
     };
 
+    const mini_search = new MiniSearch({
+        idField: 'slug',
+        fields: ['title', 'description', 'author', 'tags', 'content'],
+        storeFields: ['slug'],
+        extractField: (document, fieldName) => {
+            if (['title', 'description', 'author', 'tags'].includes(fieldName)) {
+                return document.metadata[fieldName];
+            }
+            return document[fieldName];
+        },
+    });
+    mini_search.addAll(posts);
+
+    let is_timing_out = false;
+    let last_time_out: NodeJS.Timeout;
+    const onSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        if (is_timing_out) {
+            clearTimeout(last_time_out);
+        }
+
+        if (event.target.value === '' || event.target.value.length <= 3) {
+            setDisplayedPosts(posts);
+            return;
+        }
+
+        is_timing_out = true;
+        last_time_out = setTimeout(() => {
+            let results = mini_search.search(event.target.value);
+            setDisplayedPosts(
+                posts.filter((post) => results.some((result) => result.slug === post.slug))
+            );
+            is_timing_out = false;
+        }, 650);
+    };
+
     return (
         <main className={styles.blog_page}>
             <div className={styles.filters_section}>
                 <Select categories={categories} onChange={onCategorySelect} />
+                <FullTextSearch onInput={onSearchInputChange} />
             </div>
             <div className={styles.grid_wrapper}>
                 <div className={styles.posts_grid}>
