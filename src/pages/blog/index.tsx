8651879fca259475d07900e29591632088e78f01
CarlosVilasAlvarez <carlosvilasalvarez@gmail.com>
2022-03-24 01:33:39 +0100

    handle user input with debounce

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,8 +4,9 @@ import { getAllPosts, Post } from '../../lib/blogApi';
 import styles from './index.module.scss';
 import Select from '../../components/shared/Select/Select';
 import FullTextSearch from '../../components/shared/FullTextSearch/FullTextSearch';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { PostsSearchEngineInMemory } from '../../lib/searchEngine';
+import debounce from '../../lib/debounce';
 
 type BlogHomeProps = {
     posts: Post[];
@@ -14,6 +15,8 @@ type BlogHomeProps = {
 function BlogHome({ posts }: BlogHomeProps): JSX.Element {
     const [displayed_posts, setDisplayedPosts] = useState(posts);
     const [search_engine, setSearchEngine] = useState(new PostsSearchEngineInMemory());
+    const [userIsSearching, setUserIsSearching] = useState(false);
+    console.log('rerender');
 
     // Get all the different categories and display "all" as default
     const categories = Array.from(new Set(posts.map((post) => post.metadata.tags).flat()));
@@ -35,22 +38,23 @@ function BlogHome({ posts }: BlogHomeProps): JSX.Element {
         }
     };
 
-    // Handle input in the select using at the same time the engine (timeouts used to wait for the input to be typed)
-    let typing_timeout: NodeJS.Timeout;
-    const onSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        clearTimeout(typing_timeout);
-
-        if (event.target.value === '' || event.target.value.length <= 3) {
+    // Handle user search with debounce
+    const searchHandler = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.value === '') {
             setDisplayedPosts(posts);
+            setUserIsSearching(false);
             return;
         }
-
-        typing_timeout = setTimeout(async () => {
-            let results = await search_engine.search(event.target.value);
-            setDisplayedPosts(
-                posts.filter((post) => results.some((result: any) => result.slug === post.slug))
-            );
-        }, 650);
+        let results = await search_engine.search(event.target.value);
+        setDisplayedPosts(
+            posts.filter((post) => results.some((result: any) => result.slug === post.slug))
+        );
+        setUserIsSearching(false);
+    };
+    const debouncedSearchHandler = useCallback(debounce(searchHandler, 700), []);
+    const onSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (!userIsSearching) setUserIsSearching(true);
+        debouncedSearchHandler(event);
     };
 
     return (
@@ -59,6 +63,7 @@ function BlogHome({ posts }: BlogHomeProps): JSX.Element {
                 <Select categories={categories} onChange={onCategorySelect} />
                 <FullTextSearch onInput={onSearchInputChange} />
             </div>
+            {userIsSearching && <p>LOADING</p>}
             <div className={styles.grid_wrapper}>
                 <div className={styles.posts_grid}>
                     {displayed_posts.map((post) => {
