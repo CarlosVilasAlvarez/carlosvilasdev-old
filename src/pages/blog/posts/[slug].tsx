243a339c745976cd2ecd037a4d1aa23dd310afa2
CarlosVilasAlvarez <carlosvilasalvarez@gmail.com>
2022-04-02 21:40:28 +0200

    add basic styling to post page

--- src/pages/blog/posts/[slug].tsx
+++ src/pages/blog/posts/[slug].tsx
@@ -1,5 +1,8 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
 import { getAllSlugs, getPostBySlug } from '../../../lib/blogApi';
+import styles from './post.module.scss';
 
 export default function PostPage({
     title,
@@ -9,10 +12,31 @@ export default function PostPage({
     content: string;
 }): JSX.Element {
     return (
-        <div>
-            <h1>{title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: content }}></div>
-        </div>
+        <main className={styles.post_page}>
+            <Link href="/blog">
+                <a className={styles.back_link}>
+                    <span className={styles.back_icon} role="img" aria-label="go back icon"></span>
+                    Back to articles
+                </a>
+            </Link>
+            <div className={styles.post_box}>
+                <div className={styles.post_image}>
+                    <Image
+                        src="https://via.placeholder.com/500"
+                        alt="Post cover image"
+                        layout="fill"
+                        objectFit="cover"
+                        priority
+                    />
+                </div>
+                <div className={styles.post_body}>
+                    <h1>{title}</h1>
+                    <div
+                        dangerouslySetInnerHTML={{ __html: content }}
+                        className={styles.markdown_content}></div>
+                </div>
+            </div>
+        </main>
     );
 }
 
